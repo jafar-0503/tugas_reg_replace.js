d33447fc212bb50df6e6e903205897297cd1ef64
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-18 23:15:59 +0700

    tugas_reg_replace_js
    tugas sudah ok !!!

---
+++ tugas-reg-replace.js
@@ -0,0 +1,19 @@
+// function panggilreplace(){
+//
+//   let data = "Belajar-Satu-Tahun-Bersama-Niomic";
+//   console.log(data.replace(/-/," ").replace(/-/," ").replace(/-/," ").replace(/-/," "));
+//
+// }
+// panggilreplace();
+
+
+function panggilPerulangan() {
+ var dataKota = ['Jakarta', 'Balikpapan', 'Medan'];
+ for (var i = 0; i < dataKota.length; i++) {
+   console.log(dataKota[i]);
+   // console.log(i);
+   // console.log(dataKota);
+ }
+}
+
+panggilPerulangan();
